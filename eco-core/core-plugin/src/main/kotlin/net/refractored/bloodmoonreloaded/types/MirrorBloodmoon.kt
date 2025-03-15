484db915d4077148b98b74239c23a1a70affc04a
refractored <me@refractored.net>
2025-03-15 11:59:58 -0500

    feat(MirrorBloodmoon): Warning if world or bloodmoon does not exist.

--- eco-core/core-plugin/src/main/kotlin/net/refractored/bloodmoonreloaded/types/MirrorBloodmoon.kt
+++ eco-core/core-plugin/src/main/kotlin/net/refractored/bloodmoonreloaded/types/MirrorBloodmoon.kt
@@ -7,13 +7,14 @@ import net.refractored.bloodmoonreloaded.registry.BloodmoonRegistry
 import net.refractored.bloodmoonreloaded.types.implementation.BloodmoonWorld
 import net.refractored.bloodmoonreloaded.util.MessageUtil.getStringPrefixed
 import net.refractored.bloodmoonreloaded.util.MessageUtil.miniToComponent
+import org.bukkit.Bukkit
 import org.bukkit.World
 
 /**
  * Represents a world that will start after another bloodmoon starts.
  * The "Mirrored" bloodmoon will not have the same settings.
  */
-class MirrorBloodmoon(
+class  MirrorBloodmoon(
     world: World,
     config: Config
 ) : BloodmoonWorld(world, config) {
@@ -26,6 +27,12 @@ class MirrorBloodmoon(
         .miniToComponent()
 
     val mirrorWorld = config.getString("MirrorWorld")
+
+    init {
+        if (Bukkit.getWorld(mirrorWorld) == null || BloodmoonRegistry.getWorld(mirrorWorld) == null) {
+            BloodmoonPlugin.instance.logger.warning("MirrorWorld $mirrorWorld does not exist or is not a bloodmoon world. Please check your config.")
+        }
+    }
 
     override fun shouldActivate(): Boolean {
         if (status != Status.INACTIVE) {
