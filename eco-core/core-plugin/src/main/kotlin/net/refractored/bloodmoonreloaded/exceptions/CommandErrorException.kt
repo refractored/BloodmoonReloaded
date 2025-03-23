ab7700c505a8540c0f6434475988be0feef1fee2
refractored <me@refractored.net>
2025-03-23 10:24:17 -0500

    fix(CommandErrorException): Fallback

--- eco-core/core-plugin/src/main/kotlin/net/refractored/bloodmoonreloaded/exceptions/CommandErrorException.kt
+++ eco-core/core-plugin/src/main/kotlin/net/refractored/bloodmoonreloaded/exceptions/CommandErrorException.kt
@@ -1,18 +1,20 @@
 package net.refractored.bloodmoonreloaded.exceptions
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.ComponentLike
+import net.refractored.bloodmoonreloaded.messages.Messages.toPlaintext
 import revxrsal.commands.bukkit.actor.BukkitCommandActor
 import revxrsal.commands.command.CommandActor
 import revxrsal.commands.exception.SendableException
 
 class CommandErrorException(
-    val component: Component,
-) : SendableException("") {
+    val component: ComponentLike,
+) : SendableException() {
     override fun sendTo(actor: CommandActor) {
         if (actor is BukkitCommandActor) {
             actor.reply(this.component)
         } else {
-            throw IllegalArgumentException("actor is not type BukkitCommandActor")
+            actor.reply(this.component.asComponent().toPlaintext())
         }
     }
 }
