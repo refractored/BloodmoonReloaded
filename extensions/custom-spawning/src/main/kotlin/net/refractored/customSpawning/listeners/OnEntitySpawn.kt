22fa8b62f91bdd0e6a7147bbef3c7bc055813036
refractored <me@refractored.net>
2025-04-14 15:25:54 -0500

    feat(Extensions): add new extension

---
+++ extensions/custom-spawning/src/main/kotlin/net/refractored/customSpawning/listeners/OnEntitySpawn.kt
@@ -0,0 +1,33 @@
+package net.refractored.customSpawning.listeners
+
+import net.refractored.bloodmoonreloaded.registry.BloodmoonRegistry
+import net.refractored.bloodmoonreloaded.types.implementation.BloodmoonWorld.Status
+import net.refractored.customSpawning.config.SpawnConfigRegistry
+import org.bukkit.entity.Monster
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.CreatureSpawnEvent
+
+class OnEntitySpawn : Listener {
+    @EventHandler(priority = EventPriority.HIGHEST)
+    fun onBloodmoonStart(event: CreatureSpawnEvent) {
+        val bloodmoonWorld = BloodmoonRegistry.getWorld(event.location.world.name) ?: return
+
+        if (bloodmoonWorld.status != Status.ACTIVE) return
+
+        if (event.spawnReason != CreatureSpawnEvent.SpawnReason.NATURAL) return
+
+        val spawnConfig = SpawnConfigRegistry.getSpawnConfig(event.location.world) ?: return
+
+        if (event.entity !is Monster && !spawnConfig.replaceAllMobs) return
+
+        event.isCancelled = true
+
+        if (spawnConfig.strikeLightning) {
+            event.location.world.strikeLightningEffect(event.location)
+        }
+
+        spawnConfig.getWeightedMob().spawn(event.location)
+    }
+}
