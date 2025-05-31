666867f94dc2e8c531ea950f05b1e721e9f16e1b
So4246la <102299055+So4246la@users.noreply.github.com>
2025-05-31 11:05:45 +0900

    Fix world unload listener event

--- eco-core/core-plugin/src/main/java/net/refractored/bloodmoonreloaded/listeners/OnWorldUnload.kt
+++ eco-core/core-plugin/src/main/java/net/refractored/bloodmoonreloaded/listeners/OnWorldUnload.kt
@@ -3,11 +3,11 @@ package net.refractored.bloodmoonreloaded.listeners
 import net.refractored.bloodmoonreloaded.registry.BloodmoonRegistry
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.world.WorldLoadEvent
+import org.bukkit.event.world.WorldUnloadEvent
 
 class OnWorldUnload : Listener {
     @EventHandler
-    fun execute(event: WorldLoadEvent) {
+    fun execute(event: WorldUnloadEvent) {
         BloodmoonRegistry.getWorld(event.world.name) ?: return
         BloodmoonRegistry.unregisterWorld(event.world.name)
     }
